54f1ba89997ab1c6c521ac6b26dce95b8a94d6ad
codwerk <louis_widdy@live.fr>
2021-06-01 22:07:09 +0200

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,5 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const secondSection = document.getElementById('secondSection');
-
-const menuSecond = document.getElementById('menuSecond');
-const imageFirst = document.getElementById('imageFirst');
-const imageSecond = document.getElementById('imageSecond');
-
-
 gsap.from("#menu > li", {
   duration: .5,
   opacity: 0,
@@ -14,6 +7,52 @@ gsap.from("#menu > li", {
   stagger: .2,
   delay: .5
 });
+
+gsap.from("#menuSecond > li", {
+  duration: .5,
+  opacity: 0,
+  y: -20,
+  stagger: .2,
+  delay: .8
+});
+
+gsap.from("#imageFirst", {
+  duration: .7,
+  opacity: 0,
+  y: -30,
+  delay: .5
+});
+
+gsap.from("#imageSecond", {
+  duration: .7,
+  opacity: 0,
+  y: -30,
+  delay: .6
+});
+
+gsap.from("#firstSection #heading-1", {
+  duration: .7,
+  opacity: 0,
+  y: -30,
+  delay: .6
+});
+
+gsap.from("#firstSection p", {
+  duration: .7,
+  opacity: 0,
+  y: -30,
+  delay: .7
+});
+
+
+gsap.from("#firstSection a", {
+  duration: .7,
+  opacity: 0,
+  y: -30,
+  delay: .8 
+});
+
+//scrollTrigger
 
 gsap.to("#imageFirst", {
   scrollTrigger: {
@@ -35,15 +74,110 @@ gsap.to("#imageSecond", {
   duration: 1.1,
   scale: 1.2,
   height: 380
-})
+});
 
-gsap.to("#heading-1", {
+gsap.to("#firstSection h1", {
   scrollTrigger: {
-    trigger: "#heading-1",
+    trigger: "#firstSection h1",
     scrub: true,
     start: '150% center'
   },
   color: '#fff',
   duration: 1.5
-})
+});
+
+gsap.to("#firstSection p", {
+  scrollTrigger: {
+    trigger: "#firstSection p",
+    scrub: true,
+    start: '150% center'
+  },
+  color: '#fff',
+  duration: 1.5
+});
+
+gsap.to("#firstSection a", {
+  scrollTrigger: {
+    trigger: "#firstSection a",
+    scrub: true,
+    start: '150% center'
+  },
+  color: '#fff',
+  duration: 1.5
+});
+
+gsap.to("#secondSection h1", {
+  scrollTrigger: {
+    trigger: "#secondSection h1",
+    scrub: true,
+    toggleClass: 'active',
+    start: 'top center'
+  },
+  duration: 1.5,
+  y: 40,
+  opacity: 0
+});
+
+
+gsap.to("#secondSection .component__box", {
+  scrollTrigger: {
+    trigger: "#secondSection .component__box",
+    scrub: true,
+    toggleClass: 'active',
+    start: '-10% center'
+  },
+  duration: 1.5,
+  width: 0,
+  opacity: 0
+});
+
+gsap.to("#secondSection img", {
+  scrollTrigger: {
+    trigger: "#secondSection img",
+    scrub: true,
+    toggleClass: 'active',
+    start: '-60% bottom'
+  },
+  duration: 1.5,
+  height: 0,
+  scale: 1.3,
+  opacity: 0
+});
+
+gsap.to("#secondSection .component__quote", {
+  scrollTrigger: {
+    trigger: "#secondSection .component__quote",
+    scrub: true,
+    toggleClass: 'active',
+    start: '-60% bottom'
+  },
+  duration: 1.5,
+  height: 0,
+  scale: 1.3,
+  opacity: 0
+});
+
+gsap.to("#secondSection h2", {
+  scrollTrigger: {
+    trigger: "#secondSection h2",
+    scrub: true,
+    toggleClass: 'active',
+    start: '-60% bottom'
+  },
+  duration: 1.5,
+  y: 40,
+  opacity: 0
+});
+
+gsap.to("#secondSection a", {
+  scrollTrigger: {
+    trigger: "#secondSection a",
+    scrub: true,
+    toggleClass: 'active',
+    start: '-60% bottom'
+  },
+  duration: 1.5,
+  y: 40,
+  opacity: 0
+});
 
